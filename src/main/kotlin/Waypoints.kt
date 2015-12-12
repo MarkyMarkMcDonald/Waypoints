709472d77e935fc9e05d0d8a4547393b54de7e97
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-12-12 11:28:29 +0100

    Choose between walking or public transit with naive distance

--- src/main/kotlin/Waypoints.kt
+++ src/main/kotlin/Waypoints.kt
@@ -1,20 +1,50 @@
 class Waypoints(val type: String = "", val points: List<Point> = listOf()) {
 
-    fun directions(startingLatitude: Double, startingLongitude: Double, endingLatitude: Double, endingLongitude: Double): Directions {
-
-        if (points.isNotEmpty()) {
-            return Directions(type, points[0], points[1])
-        }
-
-        return Directions("WALKING",
-                Point(startingLatitude, startingLongitude),
-                Point(endingLatitude, endingLongitude)
-        )
+    val route: Route
+    init {
+        route = Route(type, points)
     }
 
+    fun directions(startingLatitude: Double, startingLongitude: Double, endingLatitude: Double, endingLongitude: Double): Directions {
+        val start = Point(startingLatitude, startingLongitude)
+        val destination = Point(endingLatitude, endingLongitude)
 
+        val defaultDirections = Directions(Route("WALKING", listOf(start, destination)))
+
+        if (route.isNotEmpty() && publicTransitIsFaster(destination, start)) {
+            return Directions(route)
+        } else {
+            return defaultDirections
+        }
+    }
+
+    private fun publicTransitIsFaster(destination: Point, start: Point) = points.last() distanceTo destination < start distanceTo destination
 }
 
-data class Point(val latitude: Double, val longitude: Double) {}
+class Route(val type: String, val points: List<Point>) {
+    fun isNotEmpty(): Boolean {
+        return points.isNotEmpty()
+    }
+}
 
-data class Directions(val type: String, val start: Point, val end: Point) {}
+data class Point(val latitude: Double, val longitude: Double) {
+    @Deprecated("Will change to be earth based calculation in future")
+    infix fun distanceTo(end: Point): Double {
+        val latitudeDifference = this.latitude - end.latitude
+        val longitudeDifference = this.longitude - end.longitude
+        return Math.sqrt(Math.pow(latitudeDifference, 2.0) + Math.pow(longitudeDifference, 2.0))
+    }
+}
+
+class Directions(val route:Route) {
+    val end: Point
+    val start: Point
+    val type: String
+
+    init {
+        start = route.points.first()
+        end = route.points.last()
+        type = route.type
+    }
+
+}
